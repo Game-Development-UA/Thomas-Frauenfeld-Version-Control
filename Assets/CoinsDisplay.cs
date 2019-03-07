5bbc29600172b1729fe01d5cd783e084da339f84
Thomas Frauenfeld <tfrauenfeld@email.arizona.edu>
2019-03-07 10:55:37 -0700

    Made Main Menu

---
+++ Assets/CoinsDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsDisplay : MonoBehaviour {
+
+    private int coins = 0;
+    public Text coinsText;
+	
+	void Update () {
+        coinsText.text = "COINS : " + coins;
+	}
+}
